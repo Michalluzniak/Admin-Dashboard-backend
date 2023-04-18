72cf0023bfb80f6383cc32a96ba46319a02e5678
Michał Łuźniak <michal.luzniak8@gmail.com>
2023-04-18 16:24:59 +0200

    fix(main.ts): change name to index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { description, name, version } from 'package.json';
 import { ValidationPipe } from '@nestjs/common';
